c23bf478092decc5099d6655bb23fb8d060bf656
Will Smith <willsmithte@gmail.com>
2019-05-15 09:43:59 +1000

    add functionality to keep choosing new articles to view, fix a bug

--- bbcscrape.py
+++ bbcscrape.py
@@ -1,10 +1,60 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+install("requests")
+install("beautifulsoup4")
+
+class NewsItem:
+	def __init__(self, title, url):
+		self.title = title;
+		self.url = url;
+
+bbcBaseUrl = 'https://www.bbc.com/'
+
+def makeUrl(partialUrl):
+	if (bbcBaseUrl in partialUrl):
+		return partialUrl
+	else:
+		return bbcBaseUrl + partialUrl 
+
 import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-url = 'https://www.bbc.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-newsItems = soup.find_all("li", class_="media-list__item")
-print(newsItems)
+response = requests.get(bbcBaseUrl)
+homePageSoup = BeautifulSoup(response.text, "html.parser")
+rawNewsItems = homePageSoup.find_all("a", class_="media__link")
+
+newsItems = []
+for item in rawNewsItems:
+	fullUrl = makeUrl(item['href'])
+	newsItems.append(NewsItem(item.string.strip(), fullUrl));
+	print(item['href'])
+choiceExit = 'exit'
+continueChoice = ''
+choiceIndex = 'notachoice'
+
+while continueChoice != choiceExit:
+	for index, item in enumerate(newsItems):
+		print(str(index + 1) + ' - ' + item.title)
+
+	while 1:
+		try:
+			choiceIndex = int(input("Select news item index: "))
+			break
+		except ValueError:
+			print("Enter a number matching a news headline")
+
+	choice = newsItems[choiceIndex - 1]
+	itemResponse = requests.get(choice.url)
+	itemSoup = BeautifulSoup(itemResponse.text, "html.parser")
+	storyBody = itemSoup.find("div", "story-body__inner")
+	storyPTags = storyBody.find_all("p")
+	for ptag in storyPTags:
+		print("\n" + ptag.get_text())
+	continueChoice = input("\nPress any key to view headlines again, or type 'exit' to exit: ")
+
